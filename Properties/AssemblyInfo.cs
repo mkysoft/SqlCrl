e1644f301c051a2f75d6604cdeb50afdbb5bfeaf
mkysoft <mkysoft@gmail.com>
2018-04-18 11:12:32 +0300

    info added

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SqlCrl")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SQL Server CRL functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mkysoft")]
 [assembly: AssemblyProduct("SqlCrl")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
